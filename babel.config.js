806939632df691128c9bc508b23d55e859d99df7
Praveen Gupta <pragupta19@publicisgroupe.net>
2020-04-13 09:19:51 +0530

    Remove jest from dependency

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-  ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            useBuiltIns: 'entry',
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+      ],
+      plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+      ],
+    },
+  },
 };
